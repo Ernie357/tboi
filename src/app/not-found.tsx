6d846ef61861e400149636eecd4723aec0861edd
John <jmars051@live.kutztown.edu>
2025-01-29 02:50:05 -0500

    added video to 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,13 @@
 import HomeLink from "@/components/HomeLink";
 import ItemTitle from "../components/ItemTitle";
+import BackgroundVideo from "@/components/BackgroundVideo";
 
 export default function NotFound() {
     return (
-        <div className="bg-black">
+        <div className="bg-black min-h-screen w-screen flex justify-center items-center">
+            <BackgroundVideo src="/videos/i-am-error.mp4" />
             <HomeLink />
-            <div className="flex items-center justify-center min-h-screen w-full bg-isaac-crying-black-background-mobile sm:bg-black bg-no-repeat bg-contain bg-center">
-                <div className="min-h-screen w-3/4 flex justify-center items-start sm:items-center bg-transparent sm:bg-error bg-center bg-contain bg-no-repeat">
-                    <ItemTitle className="mt-64 sm:mt-0" subtitle="Page not found...">Error 404</ItemTitle>
-                </div>
-            </div>
+            <ItemTitle className="mt-64 sm:mt-0" subtitle="Page not found...">Error 404</ItemTitle>
         </div>
     );
 }
